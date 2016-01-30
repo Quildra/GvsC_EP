620e7f9923d3d5b23ad888e507dd3e8206860bb9
Quildra <Quildra@gmail.com>
2016-01-30 10:21:24 +0000

    Added django-polymorphic to the installed apps list

--- GvsC_EP/GvsC_EP/settings.py
+++ GvsC_EP/GvsC_EP/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
+    'polymorphic',
     'GvsC_Main.apps.GvscMainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
