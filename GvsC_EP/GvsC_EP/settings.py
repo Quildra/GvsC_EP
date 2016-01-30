01a3ce93b12f8f627f3617926cb4a1e04bc51c82
Quildra <Quildra@gmail.com>
2016-01-30 10:18:31 +0000

    Import the correct app

--- GvsC_EP/GvsC_EP/settings.py
+++ GvsC_EP/GvsC_EP/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gvsc_tournaments.apps.GvscMainConfig',
+
+    'GvsC_Main.apps.GvscMainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
