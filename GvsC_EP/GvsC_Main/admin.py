4a9a1483b2896e6ef43c66c4ef6ad5679a9c57eb
Quildra <Quildra@gmail.com>
2016-02-03 13:11:47 +0000

    Added in the polymorphic tournament models

--- GvsC_EP/GvsC_Main/admin.py
+++ GvsC_EP/GvsC_Main/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin, PolymorphicChildModelFilter
+from .models import Event, Tournament, SinglePlayerTournament, TeamTournament
 
-from .models import Event
+class SinglePlayerTournamentAdmin(PolymorphicChildModelAdmin):
+    base_model = SinglePlayerTournament
+    
+class TeamTournamentAdmin(PolymorphicChildModelAdmin):
+    base_model = TeamTournament
+    
+class TournamentAdmin(PolymorphicParentModelAdmin):
+    """ The parent model admin """
+    base_model = Tournament
+    list_filter = (PolymorphicChildModelFilter,)
+    child_models = (
+        (SinglePlayerTournament, SinglePlayerTournamentAdmin),
+        (TeamTournament, TeamTournamentAdmin),
+    )
 
 admin.site.register(Event)
+admin.site.register(Tournament, TournamentAdmin)
+
