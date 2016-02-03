54bb2bb4c2cbe2ca8f934e1de3a0a119de28d3a3
Quildra <Quildra@gmail.com>
2016-02-03 22:21:44 +0000

    Added a functioning login page

--- GvsC_EP/GvsC_Main/urls.py
+++ GvsC_EP/GvsC_Main/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^accounts/profile/$', views.profile, name='profile'),
+    url(r'^events/$', views.events_index, name='index'),
+    # ex: /events/5/
+    #url(r'^events/(?P<event_id>[0-9]+)/$', views.events_details, name='index'),
+    # ex: /tournaments/
+    url(r'^tournaments/$', views.tournaments_index, name='index'),
+    # ex: /tournaments/5/
+    #url(r'^tournaments/(?P<tournament_id>[0-9]+)/$', views.tournaments_details, name='index'),
 ]
--- GvsC_EP/GvsC_Main/views.py
+++ GvsC_EP/GvsC_Main/views.py
@@ -2,9 +2,24 @@ from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
-from .models import Event, SinglePlayerTournament
+from .models import Event, Tournament
 
 def index(request):
     upcoming_events = Event.objects.order_by('-start_date')
     context = {'upcoming_events': upcoming_events}
     return render(request, 'index.html', context)
+
+def profile(request):
+    #upcoming_events = Event.objects.order_by('-start_date')
+    context = {}#{'upcoming_events': upcoming_events}
+    return render(request, 'profile.html', context)
+
+def events_index(request):
+    upcoming_events = Event.objects.order_by('-start_date')
+    context = {'upcoming_events': upcoming_events}
+    return render(request, 'events/index.html', context)
+
+def tournaments_index(request):
+    upcoming_tournaments = Tournament.objects.order_by('+id')
+    context = {'upcoming_tournaments': upcoming_tournaments}
+    return render(request, 'tournaments/index.html', context)
