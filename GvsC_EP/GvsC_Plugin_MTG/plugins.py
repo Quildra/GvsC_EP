2e8a64699ee13c062e974dab2c1a0f53fbb44ea9
Quildra <Quildra@gmail.com>
2016-02-16 17:14:30 +0000

    Fleshed out the table generation a bit more.
    Added random stats for the moment while the matching and reporting is
    still being written.

--- GvsC_EP/GvsC_Plugin_MTG/plugins.py
+++ GvsC_EP/GvsC_Plugin_MTG/plugins.py
@@ -1,12 +1,35 @@
 from __future__ import absolute_import
+from random import randint
 
 from GvsC_EP.plugins import GamePluginPoint
 
 class MTG_GamePlugin(GamePluginPoint):
     name = 'MTG'
     title = 'Magic: The Gathering'
+    
+    def _CalcualtePlayerStats(self, pPlayer):
+        player = {}
+        
+        player['name'] = pPlayer.name
+        player['match_points'] = randint(0,10)
+        player['opp_match_win_percent'] = 0
+        player['game_win_percent'] = 0
+        player['opp_game_win_percent'] = 0
+        player['byes'] = 0
+        
+        return player
 
     def GenerateStandingsTable(self, pTournament):
+        single_player_tournament = hasattr(pTournament, 'players')
+        players = []
+        if single_player_tournament:
+            for i, player in enumerate(pTournament.players.all()):
+                # Calculate the points and tie breakers for each player
+                # store them locally to be sorted.
+                players.append(self._CalcualtePlayerStats(player))
+                
+        # Sort the players
+        players.sort(key = lambda player: (player['match_points'], player['opp_match_win_percent'], player['game_win_percent'],player['opp_game_win_percent'], player['byes'], player['name']), reverse=True)        
         table_string = ''
         table_string += '<table class="ui celled padded table">\r\n'
         table_string += '\t<thead>\r\n'
@@ -17,12 +40,13 @@ class MTG_GamePlugin(GamePluginPoint):
         table_string += '\t\t<tr>\r\n'
         table_string += '\t</thead>\r\n'
         table_string += '\t<tbody>\r\n'
-        if hasattr(pTournament, 'players'):
-            for i, player in enumerate(pTournament.players.all()):
+        if single_player_tournament:
+            # Report the players in sorted order.
+            for i, player in enumerate(players):
                 table_string += '\t\t<tr>\r\n'
                 table_string += '\t\t\t<td>' + str(i+1) + '</td>\r\n'
-                table_string += '\t\t\t<td>' + player.name + '</td>\r\n'
-                table_string += '\t\t\t<td>' + str(0) + '</td>\r\n'
+                table_string += '\t\t\t<td>' + player['name'] + '</td>\r\n'
+                table_string += '\t\t\t<td>' + str(player['match_points']) + '</td>\r\n'
                 table_string += '\t\t</tr>\r\n'
 
         table_string += '\t</tbody>\r\n'
