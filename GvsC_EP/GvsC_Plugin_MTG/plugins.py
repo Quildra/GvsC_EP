b43ea44aaebe76e452d688f801bc231e52462592
Quildra <>
2016-05-08 16:53:11 +0100

    - Documented the pairing algorithm  - Adjusted for a mistake in while looping through possible opponents.

--- GvsC_EP/GvsC_Plugin_MTG/plugins.py
+++ GvsC_EP/GvsC_Plugin_MTG/plugins.py
@@ -127,55 +127,75 @@ class MTG_GamePlugin(GamePluginPoint):
                 # store them locally to be sorted.
                 if not participant.dropped:
                     participants.append(self._calcualte_player_stats(participant, pTournament))
-                
-        # Sort the players
-        pairings = []        
+
         groups = []
         uniquekeys = []
+        # Sort by wins then draws then losses then byes in decending order.
         data = sorted(participants, key=lambda participant: (participant['wins'], participant['draws'], participant['losses'], participant['byes']), reverse=True)
+        # Groupby splits one list into a list of lists , grouping like for like.
+        # For example if you had a list of players with wins and losses [{p['n']:'a', p['w']:2, p['l']:0}, {p['n']:'b', p['w']:1, p['l']:2}, {p['n']:'c', p['w']:2, p['l']:0}, {p['n']:'d', p['w']:2, p['l']:1}, {p['n']:'e', p['w']:1, p['l']:2}]
+        # You would get this back [ [ {p['n']:'a', p['w']:2, p['l']:0}, {p['n']:'c', p['w']:2, p['l']:0} ], [ {p['n']:'d', p['w']:2, p['l']:1} ], [ {p['n']:'b', p['w']:1, p['l']:2}, {p['n']:'e', p['w']:1, p['l']:2} ] ]
         for k, g in groupby(data, lambda participant: (participant['wins'], participant['draws'], participant['losses'])):
             groups.append(list(g))
             uniquekeys.append(k)
             
-        print(groups)
-        
+        #print(groups)
+        pairings = []
+
+        # Now loop through each group of players on the same record and try to match them up as best you can.
         for i, group in enumerate(groups):
+            # If the group has more than 1 player in it then we are good to try and match 2 players from that group
             while len(group) > 1:
+                # Grab the first person in the list.
+                # Seen as players with byes will be sorted before players without byes this lets us ensure that
+                # A player recieves as few byes as possible.
+                # Note this will need re-working for "first-round byes" and possibly over long tournaments, might need to identify byes first and remove them from the pool.
                 participant_1_index = 0
                 participant_1 = group[participant_1_index]
                 group.remove(participant_1)
-                # Make a group of possible opponents from the current group of players
-                possible_opponents = list(group)
-                possible_opponent_group = i
+                # Make a group of remaining opponents from the current group of players excluding the player being paired.
+                remaining_opponents = list(group)
+                # Keep track of the group we are looping through as we might need to change groups is there is no viable match in it.
+                # This can happen in later rounds.
+                remaining_opponent_group = i
+                possible_opponents = []
                 while True:
                     # Loop through the list
-                    for possible in possible_opponents:
-                        # See if they are in my opponents list.
-                        if TournamentParticipantOpponent.objects.filter(current_player=participant_1['player'], opponent_player=possible['player']).count() > 0:
-                            possible_opponents.remove(possible)
+                    for possible in remaining_opponents:
+                        # See if they are in my opponents list. If they are then I have played them in a previous round and we ignore them, if i haven't then we add them to the list for consideration
+                        if TournamentParticipantOpponent.objects.filter(current_player=participant_1['player'], opponent_player=possible['player']).count() == 0:
+                            possible_opponents.append(possible)
 
+                    # If I still have some possible opponents left in the current group, great!
+                    # Lets break out of the loop and pick one of them
                     if len(possible_opponents) > 0:
-                        break;
+                        break
+                    # If I don't then we need to look at the next group of players and start again
                     else:
-                        possible_opponent_group += 1
-                        num_groups = len(groups)
-                        if possible_opponent_group >= num_groups:
+                        remaining_opponent_group += 1
+                        if remaining_opponent_group >= len(groups):
                             return GvsC_Main.errors.ERROR_NO_VIABLE_MATCHES, None
-                        possible_opponents = list(groups[possible_opponent_group])
+                        remaining_opponents = list(groups[remaining_opponent_group])
+                        possible_opponents = []
 
-                participant_2_index = randint(0,len(possible_opponents)-1)
-                participant_2 = groups[possible_opponent_group][participant_2_index]
+                # Randomly pick a player from the remaining players
+                # This isn't true swiss at this point but true swiss needs to be played with a power of 2 number of people.
+                participant_2_index = randint(0, len(possible_opponents) - 1)
+                participant_2 = possible_opponents[participant_2_index]
 
-                output = str(participant_1) + " Vs " + str(participant_2)
-
+                # Add this pairing to the list of pairings I've already made
                 pairings.append([participant_1, participant_2])
-                
 
-                groups[possible_opponent_group].remove(participant_2)
-            
-            if len(group) == 1 and i+1 < len(groups):
-                groups[i+1].insert(0, group[0])
+                # Remove the matched player from the group they belong to.
+                # Pythong will see to objects as the same if they have the same data in them so I can use the object from a differnt list to do this.
+                groups[remaining_opponent_group].remove(participant_2)
+
+            # If i have 1 player left in my list and we still have atleast 1 more group of players move the last player to the front of the next group
+            # They will get matched first this way, although they might mess-up the byes like this.
+            if len(group) == 1 and i + 1 < len(groups):
+                groups[i + 1].insert(0, group[0])
                 group.remove(group[0])
+            # If we are in the last group and have 1 player left over then give them the bye by pairing them with "None"
             elif len(group) == 1:
                 pairings.append([group[0], None])
                 
